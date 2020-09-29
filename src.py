69986836e50d940c17381b25ef6cc09f564a82e8
Iqra840 <54781026+Iqra840@users.noreply.github.com>
2020-09-30 03:53:50 +0500

    Update src.py

--- src.py
+++ src.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 
+#viewing image before and after applying filters
 def viewImage(image):
     cv2.namedWindow('Display', cv2.WINDOW_NORMAL)
     cv2.imshow('Display', image)
@@ -33,7 +34,7 @@ peach_hsv = cv2.cvtColor(peach,cv2.COLOR_BGR2HSV)
 #print(peach_hsv)
 image = cv2.imread('/Users/iqra/Desktop/img2.jpg')
 hsv_img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-viewImage(hsv_img) ## 1
+
 peach_low = np.array([45 , 100, 50] )
 peach_high = np.array([75, 255, 255])
 curr_mask = cv2.inRange(hsv_img, peach_low, peach_high)
@@ -43,12 +44,12 @@ viewImage(hsv_img) ## 2
 ## contouring
 RGB_again = cv2.cvtColor(hsv_img, cv2.COLOR_HSV2RGB)
 gray = cv2.cvtColor(RGB_again, cv2.COLOR_RGB2GRAY)
-viewImage(gray) ## 3
+
 ret, threshold = cv2.threshold(gray, 90, 255, 0)
-viewImage(threshold) ## 4
+
 contours, hierarchy =  cv2.findContours(threshold,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 cv2.drawContours(image, contours, -1, (0, 0, 255), 3)
-viewImage(image) ## 5
+viewImage(image) 
 # to get the center of the contour
 cnt = contours[13]
 M = cv2.moments(cnt)
